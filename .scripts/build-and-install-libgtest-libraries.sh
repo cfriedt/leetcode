111d4de782283c21e2598fbb53d613c3d9954c06
Christopher Friedt <chrisfriedt@gmail.com>
2018-11-29 12:23:01 -0500

    this is getting annoying..

--- .scripts/build-and-install-libgtest-libraries.sh
+++ .scripts/build-and-install-libgtest-libraries.sh
@@ -20,11 +20,11 @@ cmake -DBUILD_SHARED_LIBS=ON || E "cmake -DBUILD_SHARED_LIBS=ON failed"
 make || E make failed
 cp *.so /usr/lib/ || E failed to copy .so files to /usr/lib
 
-cd /usr/src/gtest || E failed to change to /usr/src/gtest
-make clean || E failed to clean source directory
-rm -Rf CMakeCache.txt CMakeFiles/ || E failed to clean cmake files
-cmake || E cmake failed
-make || E make failed
-cp *.a /usr/lib/ || E failed to copy .a files to /usr/lib
+#cd /usr/src/gtest || E failed to change to /usr/src/gtest
+#make clean || E failed to clean source directory
+#rm -Rf CMakeCache.txt CMakeFiles/ || E failed to clean cmake files
+#cmake || E cmake failed
+#make || E make failed
+#cp *.a /usr/lib/ || E failed to copy .a files to /usr/lib
 
 
