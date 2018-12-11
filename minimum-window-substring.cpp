d7582b9169a3002ade0731feceb17a7f0b2518d9
Christopher Friedt <chrisfriedt@gmail.com>
2018-12-11 08:03:22 -0500

    Implement minimum-window-substring
    This implementation accounts for duplicates in t.
    
    time: 12ms
    rank: 68.14%
    
    time complexity: O( N )
    space complexity: O( 1 ) (fixed-size LUTs)

--- minimum-window-substring.cpp
+++ minimum-window-substring.cpp
@@ -22,10 +22,8 @@
  * SOFTWARE.
  */
 
-#include <map>
-#include <set>
-#include <string>
-#include <unordered_set>
+#include <algorithm>
+#include <array>
 #include <vector>
 
 using namespace std;
@@ -37,131 +35,115 @@ public:
 
 	string minWindow(string s, string t) {
 
-		// This is a slightly deceptive problem when first encountered.
-		// Outwardly, it doesn't *seem* like it's a difficult problem until you try to solve it.
 		//
-		// However, it is *extremely* important in this day and age, particularly when
-		// you consider that it is exactly like another problem, not necessarily on
-		// leetcode.com, but encountered *billions* of times daily.
-		// That problem is, namely, the following:
+		// This is attempt 2 using the sliding window approach. It's basically the same
+		// approach that I took before except it handles duplicates in t.
 		//
-		// Given a page of text (i.e. words separated by whitespace in string form), and another
-		// string containing a number of search terms separated by whitespace, find the shortest
-		// window of text containing all search terms. This is categorically Google.
+		// The algorithmic complexity in minimum-window-substring.README.txt is wrong
+		// but I jotted down a number of interesting notes there.
 		//
-		// Input: S = "ADOBECODEBANC", T = "ABC"
-		// Output: "BANC"
+		// If we look at the algorithmic complexity of this approach, it's
+		// at least O( N ), where N is the number of characters in s. In the worst-case
+		// scenario (where all of the search terms are at the end of s), it is ~O( 2N )
+		// because we would grow the window to the size of s and then shrink it to
+		// N - M.
 		//
-		// The solution that I would consider "brute force" is the one that I thought of first,
-		// which is the following:
-		//                               shortest
-		// i    A_i    B_i    C_i     window (pos, len)
-		// --------------------------------------------
-		// 0     0
-		// ..
-		// 3     0      3
-		// ..
-		// 5     0      3      5       (0,6)
-		// ..
-		// 9     0      9      5       (0,6) ** shortest window not updated, because (0,10) is longer
-		// 10    10     9      5       (0,6) ** shortest window not updated, because (5,6) is longer
-		//                                      N.B. We should probably set out some assumptions
-		//                                           - only one shortest window exists (i.e. there is only 1 correct answer)
-		//                                           - all items in T are unique.
-		//                                      N.B. Some shortcuts
-		//                                           - if window.length() == T.length(), short circuit return window
-		//                                           - if S.length() < T.length() there is no way S can contain all terms in T, so return ""
-		// ..
-		// 12    10     9      12      (9,4)
-		//
-		// Reached the end, and have found the shortest term, in this case.
-		// This algorithm is ok-ish, at first glance, it appears to be, it's O( N * M ), where N is the number of items in S
-		// and M is the number of items in T. In the degenerate case where M is small, it's linear though, so not all hope is lost!
-		// ======================================================================================
-		// Correction!!! It's O( N log M ) approximately, although the details are revealed below
-		// ======================================================================================
-		//
-		// Let's assume that other people are convinced that it should be coded-up, in any case.
-		//
-		// We obviously can't hard-code variables A_i, B_i, and C_i, but what I would do is use an unordered_map<char,size_t> to
-		// keep track of them. If the size of the map is equal to the number of items in T, then we know we have found a solution.
-		// ======================================================================================
-		// Correction!!! Use an ordered map (std::map) to keep track of the indices by char, but
-		// also use an ordered set (std::set) to keep the indices in order as well.
-		// ======================================================================================
-		//
-		// For keeping track of the shortest window, we can use two variables: size_t window_pos, size_t window_len.
-		// If we initialize window_len to S.length(), then we can be sure we will always be updating it with a smaller size.
-		//
-		// Finally, let's also not forget the short circuit solutions we mentioned above.
-		//
-		// Additional comments:
-		//
-		// The leetcode test cases allows duplicates in t, which is super freaking annoying.
-		// It's an unnecessarily complex wrench to throw into the works, but realistically, so is
-		// every other mutation of a common problem.
 
-		if ( s.length() < t.length() ) {
-			// short circuit return
+		if ( s.empty() || t.empty() || s.length() < t.length() ) {
 			return "";
 		}
 
-		if ( s == t ) {
-			// short circuit return
-			return t;
+		// t_count is a LUT so that we can use to see if a given char in s is a term in t.
+		// Really it's like an unordered_map<char,size_t>.
+		// We likely waste a fair amount of space by using a LUT. In this case, it's 1kB
+		// which can be significant on small platforms. It's fast though.
+		// If we were to expand this algorithm to s and t as string vectors, or any arbitrary
+		// key_type, an unordered_map<key_type,size_t> would be preferred.
+		// N.B. the reason we don't simply use e.g. an unordered_set<char> here is to account
+		// for duplicates in t.
+		array<size_t,256> t_count;
+		fill( t_count.begin(), t_count.end(), 0 );
+
+		// we keep track of the number of unique terms in t. once we have matched the
+		// t_count in s_count for all unique items in t, then we have found a valid window.
+		size_t n_t_unique = 0;
+
+		// populate the t_count LUT with items in t, and also count the unique items in t
+		for( auto c: t ) {
+			if ( 0 == t_count[ c ] ) {
+				n_t_unique++;
+			}
+			t_count[ c ]++;
 		}
 
-		unordered_set<char> terms( t.begin(), t.end() ); // uses a hash table
+		// here we maintain a running count (also via LUT) of the characters in s contained
+		// within our window.
+		array<size_t,256> s_count;
+		fill( s_count.begin(), s_count.end(), 0 );
 
-		map<char,size_t> indices; // uses a self-balancing tree (e.g. red-black) is used
-		set<size_t> by_index; // uses a self-balancing tree (e.g. red-black) is used
+		// keep track of the smallest window position and length
+		size_t win_pos = 0;
+		size_t win_len = SIZE_MAX;
 
-		size_t window_pos = 0;
-		size_t window_len = s.length();
+		size_t L, R, t_matched;
+		for( L = 0, R = 0, t_matched = 0; R < s.size(); ) {
 
-		for( size_t i = 0; i < s.length(); i++ ) { // O( N )
-
-			// if the char at position i is one of our search terms..
-			if ( terms.end() != terms.find( s[ i ] ) ) { // O( 1 )
-
-				auto it = indices.find( s[ i ] ); // O( log M )
-
-				if ( it == indices.end() ) {
-
-					by_index.insert( i ); // O( log M )
-
-					// update the most recent occurrence of character s[i]
-					indices[ s[ i ] ] = i; // O( log M )
-				} else {
-
-					by_index.erase( it->second ); // O( log M )
-					by_index.insert( i ); // O( log M )
-
-					// update the most recent occurrence of character s[i]
-					it->second = i; // O( 1 ), since we saved the iterator
+			// since we initialize L and R at zero, we have to pre-fill s_count if s[ 0 ] is a term in t
+			if ( 0 == L && 0 == R ) {
+				if ( t_count[ s[ 0 ] ] ) {
+					s_count[ s[ 0 ] ] = 1;
+					if ( s_count[ s[ 0 ] ] == t_count[ s[ 0 ] ] ) {
+						// we have exactly the number of s[ 0 ] in our window that exist in t
+						t_matched++;
+					}
 				}
+			}
 
-				if ( indices.size() == terms.size() ) { // O( 1 )
-					// update window position, length, if appropriate
-					size_t new_window_begin = *(by_index.begin());
-					size_t new_window_end = *(by_index.rbegin());
-					size_t new_window_len = new_window_end - new_window_begin + 1;
-					if ( new_window_len < window_len ) {
-						window_pos = new_window_begin;
-						window_len = new_window_len;
-						if ( window_len == t.length() ) {
-							// short circuit return
+			if ( t_matched < n_t_unique ) {
+				// increase R to widen the search window
+				R++;
+				if ( R >= s.size() ) {
+					// do not segfault
+					break;
+				}
+				if ( t_count[ s[ R ] ] > 0 ) {
+					// s[ R ] is a term in t, so increase the count within our widened search window
+					s_count[ s[ R ] ]++;
+					if ( s_count[ s[ R ] ] == t_count[ s[ R ] ] ) {
+						// we have exactly the number of s[ R ] in our window that exist in t
+						t_matched++;
+					}
+				}
+			} else if ( t_matched >= n_t_unique ) {
+				if ( t_matched == n_t_unique ) {
+					// we have found an "interesting" window
+					size_t new_win_pos = L;
+					size_t new_win_len = (R-L)+1;
+					if ( new_win_len < win_len ) {
+						win_pos = new_win_pos;
+						win_len = new_win_len;
+
+						if ( t.length() == win_len ) {
+							// the answer cannot get any smaller than this so, assuming
+							// "there can be only one" (answer), return the newly found window.
 							break;
 						}
 					}
 				}
+
+				// increase L to narrow the search window
+				if ( t_count[ s[ L ] ] > 0 ) {
+					// s[ L ] was a term in t, so decrease the count within our narrowing search window
+					if ( s_count[ s[ L ] ] == t_count[ s[ L ] ] ) {
+						// we no longer will have exactly the number of s[ L ] in our window that exist in t
+						t_matched--;
+					}
+					s_count[ s[ L ] ]--;
+				}
+				L++;
 			}
 		}
 
-		if ( indices.size() == terms.size() ) {
-			return s.substr( window_pos, window_len );
-		} else {
-			return "";
-		}
+		return ( SIZE_MAX == win_len ) ? "" : s.substr( win_pos, win_len );
 	}
 };
