195261c3be94f655b0e8b1ad9327053310c13dcd
Christopher Friedt <chrisfriedt@gmail.com>
2019-01-26 16:51:17 -0500

    Need to work on this
    For some reason, my original solution failed.
    
    It looks as though this guy has a correct one
    
    https://goo.gl/6P2Sj7
    
    But although his code is clean, it's not very easy to understand.

--- insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,134 +1,46 @@
-/*
- * MIT License
- *
- * Copyright (c) 2018 Christopher Friedt
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-#include <cmath>
-#include <random>
 #include <unordered_map>
 #include <utility>
 #include <vector>
 
 using namespace std;
 
-// https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/
-
 class RandomizedCollection {
 public:
-    RandomizedCollection() : nunique_items( 0 ), gen(rd()) {}
+    /** Initialize your data structure here. */
+    RandomizedCollection() {
 
-    using size_ptr = pair<size_t,int *>; // count, and int *
+    }
 
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     bool insert(int val) {
-    	bool r;
-    	auto it = elements.find( val );
-    	if ( elements.end() == it ) {
-    		vec.push_back( val );
-    		size_ptr sp = size_ptr( 1, & vec.back() );
-    		elements[ val ] = sp;
-    		r = true;
-    		nunique_items++;
-    	} else {
-    		size_t & count = it->second.first;
-    		bool present = count > 0;
-    		if ( ! present ) {
-    			nunique_items++;
-    		}
-    		r = ! present;
-    		count++;
-    	}
-    	return r;
+        auto result = m.find(val) == m.end();
+
+        m[val].push_back(nums.size());
+        nums.push_back(pair<int, int>(val, m[val].size() - 1));
+
+        return result;
     }
 
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     bool remove(int val) {
-    	bool r;
-    	auto it = elements.find( val );
-    	if ( elements.end() == it ) {
-    		r = false;
-    	} else {
-    		size_t & count = it->second.first;
-    		bool present = count > 0;
-    		r = present;
-    		if ( present ) {
-    			count--;
-        		if ( 0 == count ) {
-        			nunique_items--;
-        		}
-    		}
-    	}
-    	return r;
+        auto result = m.find(val) != m.end();
+        if(result)
+        {
+            auto last = nums.back();
+            m[last.first][last.second] = m[val].back();
+            nums[m[val].back()] = last;
+            m[val].pop_back();
+            if(m[val].empty()) m.erase(val);
+            nums.pop_back();
+        }
+        return result;
     }
 
-    size_t getRandomIndex() {
-    	if ( 0 == nunique_items ) {
-    		throw logic_error( "cannot get a random element from a container of size 0" );
-    	}
-
-    	size_t n = vec.size();
-    	uniform_int_distribution<size_t> dis(0,n-1);
-
-    	return dis( gen );
-    }
-
-    /** Get a random element from the set. */
+    /** Get a random element from the collection. */
     int getRandom() {
-
-    	int val;
-
-    	for( ;; ) {
-    		size_t idx = getRandomIndex();
-    		val = vec[ idx ];
-    		size_t & count = elements[ val ].first;
-    		bool present = count > 0;
-    		if ( present ) {
-    			break;
-    		}
-    	}
-
-    	return val;
+        return nums[rand() % nums.size()].first;
     }
-
-    size_t getNumUniqueItems() {
-    	return nunique_items;
-    }
-
-protected:
-    unordered_map<int,size_ptr> elements;
-    vector<int> vec;
-    size_t nunique_items;
-
-	// Very c++11-ish way of generating random values.
-	// In a pinch, I would probably just use random().
-
-	random_device rd;
-	mt19937 gen;
+private:
+    vector<pair<int, int>> nums;
+    unordered_map<int, vector<int>> m;
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * bool param_1 = obj.insert(val);
- * bool param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
