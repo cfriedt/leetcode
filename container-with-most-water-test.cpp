041e2c4f238e8308d478cd4178a4654a2b2d5775
Christopher Friedt <chrisfriedt@gmail.com>
2019-01-28 23:13:25 -0500

    Implement container-with-most-water
    https://leetcode.com/problems/container-with-most-water
    
    time: 12ms
    time-rank: 98.86%
    time-complexity: O(N)
    
    space: 9.6MB
    space-rank: 100%
    space-complexity: O(1)

---
+++ container-with-most-water-test.cpp
@@ -0,0 +1,36 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <gtest.h>
+
+using namespace std;
+
+#include "container-with-most-water.cpp"
+
+TEST( ContainerWithMostWater, Test_1_8_6_2_5_4_8_3_7 ) {
+	vector<int> height({1,8,6,2,5,4,8,3,7});
+	int expected_int = 49;
+	int actual_int = Solution().maxArea(height);
+	EXPECT_EQ(actual_int, expected_int);
+}
