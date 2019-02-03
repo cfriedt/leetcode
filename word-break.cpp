05add308cfce973209e68e4a00ce55568c69931b
Christopher Friedt <chrisfriedt@gmail.com>
2019-02-02 23:20:28 -0500

    Implement word-break
    https://leetcode.com/problems/word-break
    
    time: 0ms
    time-rank: 100%
    time-complexity: O( N^2 )
    
    space: 10.7MB
    space-rank: 71.70%
    space-complexity: O( N )
    
    Wish I could say that I came up with this solution. Found here:
    
    https://leetcode.com/problems/word-break/discuss/43814/C%2B%2B-Dynamic-Programming-simple-and-fast-solution-(4ms)-with-optimization
    
    However, I am not really satisfied that this is the best solution in terms of time complexity. It is fairly minimal in terms of
    space complexity.
    
    In my own implementation, I was off implementing something with Tries, and started reading about Suffix Trees... not sure if it's fully applicable
    here, but I thought I could achieve this in O( N ) in both time and space.
    
    Radix trees, (patricia trees, radix tries) also are able to save some space when compared to regular Tries.
    
    https://en.wikipedia.org/wiki/Radix_tree

---
+++ word-break.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/word-break/
+// https://leetcode.com/problems/word-break/discuss/43814/C%2B%2B-Dynamic-Programming-simple-and-fast-solution-(4ms)-with-optimization
+
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool wordBreak(const string & s, const vector<string> & wordDict) {
+		unordered_set<string> wd( wordDict.begin(), wordDict.end() );
+		return wordBreak( s, wd );
+	}
+protected:
+	bool wordBreak(const string & s, const unordered_set<string>& wordDict) {
+        if(wordDict.size()==0) return false;
+
+        vector<bool> dp(s.size()+1,false);
+        dp[0]=true;
+
+        for(int i=1;i<=int(s.size());i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(dp[j])
+                {
+                    string word = s.substr(j,i-j);
+                    if(wordDict.find(word)!= wordDict.end())
+                    {
+                        dp[i]=true;
+                        break; //next i
+                    }
+                }
+            }
+        }
+
+        return dp[s.size()];
+	}
+};
