db2f0bb0285c2cfae5295dab81c7e40e1dcd5f58
Christopher Friedt <chrisfriedt@gmail.com>
2019-02-12 01:12:14 -0500

    Implement the-maze-iii
    https://leetcode.com/problems/the-maze-iii
    
    time: 84ms
    time-rank: 30%
    time-complexity: O( |E| |V|log|V| ), where E is the number of edges  and V is the number of vertices
    
    space: 21.2MB
    space-rank: 0%
    space-complexity: O( MN ) where M is the number of rows and N is the number of columns

---
+++ the-maze-iii-test.cpp
@@ -0,0 +1,349 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <gtest.h>
+
+#include "the-maze-iii.cpp"
+
+TEST( TheMazeIii, Test_lul ) {
+	vector<vector<int>> maze({
+		{0,0,0,0,0},
+		{1,1,0,0,1},
+		{0,0,0,0,0},
+		{0,1,0,0,1},
+		{0,1,0,0,0},
+	});
+	vector<int> ball({4,3});
+	vector<int> hole({0,1});
+
+	string expected_string = "lul";
+	string actual_string = Solution().findShortestWay( maze, ball, hole );
+
+	EXPECT_EQ( actual_string, expected_string );
+}
+
+// UGH - this question is brutal!!!
+// Need to throw in another check to see if
+// there is a wall whenever changing directions!!!
+TEST( TheMazeIii, Test_impossible ) {
+	vector<vector<int>> maze({
+		{0,0,0,0,0},
+		{1,1,0,0,1},
+		{0,0,0,0,0},
+		{0,1,0,0,1},
+		{0,1,0,0,0},
+	});
+	vector<int> ball({4,3});
+	vector<int> hole({3,0});
+
+	string expected_string = "impossible";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+// this one has a cycle in one of the possible paths
+TEST( TheMazeIii, Test_r ) {
+	vector<vector<int>> maze({
+		{0,0,1,0,0},
+		{0,0,0,0,0},
+		{0,0,0,1,0},
+		{1,1,0,1,1},
+		{0,0,0,0,0},
+	});
+	vector<int> ball({1,1});
+	vector<int> hole({1,2});
+
+	string expected_string = "r";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+// AUGAAHHGGHH!!! This is nuts!
+// So the problem here, is likely that you *can* have a kind of cycle - the kind
+// where you arrive at the same path space with a different direction!!!!
+// So I need to change BreadCrumbs to be a pair<Node*,int> (int is a direction).
+TEST( TheMazeIii, Test_cycle_with_different_dir ) {
+	vector<vector<int>> maze({
+		{0,1,0,1,0,0,0,0,0,0,1},
+		{0,1,0,1,1,1,0,1,1,0,0},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{0,0,0,1,1,1,0,1,0,0,1},
+		{1,1,0,0,0,1,0,0,0,1,1},
+		{0,1,0,0,0,0,0,1,0,1,0},
+		{0,0,0,0,1,0,0,1,1,1,0},
+	});
+	vector<int> ball({0,4});
+	vector<int> hole({1,6});
+
+	string expected_string = "rdluldrdrurdru";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+TEST( TheMazeIii, Test_paths_that_cross ) {
+	vector<vector<int>> maze({
+		{0,1,0,0,0},
+		{0,1,0,1,0},
+		{0,0,0,0,0},
+		{1,1,0,1,1},
+		{1,1,0,1,1},
+	});
+	vector<int> ball({4,2});
+	vector<int> hole({0,0});
+
+	string expected_string = "urdlu";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+TEST( TheMazeIii, Test_naiive_dfs_will_not_cut_it ) {
+	vector<vector<int>> maze({
+		{0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1},
+		{0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0},
+		{1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1},
+		{0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1},
+		{0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0},
+		{1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0},
+		{0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0},
+		{0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1},
+		{0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0},
+		{1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0},
+		{0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1},
+		{0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0},
+		{1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0},
+		{0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1},
+		{0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0},
+		{1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0},
+		{0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1},
+		{0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0},
+		{0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0},
+	});
+	vector<int> ball({4,3});
+	vector<int> hole({10,27});
+
+	string expected_string = "drdrdrdrdrdru";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+/*
+TEST( TheMazeIii, Test_size_zero ) {
+	vector<vector<int>> maze;
+	vector<int> ball;
+	vector<int> hole;
+
+	string expected_string = "impossible";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+TEST( TheMazeIii, Test_size_one ) {
+	vector<vector<int>> maze({{0}});
+	vector<int> ball({0,0});
+	vector<int> hole({0,0});
+
+	string expected_string = "";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+TEST( TheMazeIii, Test_invalid_ball ) {
+	vector<vector<int>> maze({
+		{0,0,0,0,0},
+		{1,1,0,0,1},
+		{0,0,0,0,0},
+		{0,1,0,0,1},
+		{0,1,0,0,0},
+	});
+	vector<int> ball({50,50});
+	vector<int> hole({0,0});
+
+	string expected_string = "impossible";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+TEST( TheMazeIii, Test_no_ball ) {
+	vector<vector<int>> maze({
+		{0,0,0,0,0},
+		{1,1,0,0,1},
+		{0,0,0,0,0},
+		{0,1,0,0,1},
+		{0,1,0,0,0},
+	});
+	vector<int> ball;
+	vector<int> hole({0,0});
+
+	string expected_string = "impossible";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+TEST( TheMazeIii, Test_invalid_hole ) {
+	vector<vector<int>> maze({
+		{0,0,0,0,0},
+		{1,1,0,0,1},
+		{0,0,0,0,0},
+		{0,1,0,0,1},
+		{0,1,0,0,0},
+	});
+	vector<int> ball({0,0});
+	vector<int> hole({50,50});
+
+	string expected_string = "impossible";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+TEST( TheMazeIii, Test_no_hole ) {
+	vector<vector<int>> maze({
+		{0,0,0,0,0},
+		{1,1,0,0,1},
+		{0,0,0,0,0},
+		{0,1,0,0,1},
+		{0,1,0,0,0},
+	});
+	vector<int> ball({0,0});
+	vector<int> hole;
+
+	string expected_string = "impossible";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+*/
+
+TEST( TheMazeIii, Test_dldl ) {
+	vector<vector<int>> maze({
+		{0,0,0,0,0,0,0},
+		{0,0,1,0,0,1,0},
+		{0,0,0,0,1,0,0},
+		{0,0,0,0,0,0,1},
+	});
+	vector<int> ball({0,4});
+	vector<int> hole({3,0});
+
+	string expected_string = "dldl";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+/*
+TEST( TheMazeIii, Test_too_big ) {
+	vector<vector<int>> maze(300,vector<int>(300));
+	vector<int> ball({1,1});
+	vector<int> hole({0,0});
+
+	string expected_string = "impossible";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+*/
+
+TEST( TheMazeIii, Test_rulululurdrulululululdrulululul ) {
+	vector<vector<int>> maze({
+		{0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0},
+		{0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1},
+		{0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0},
+		{0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0},
+		{0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0},
+		{0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0},
+		{1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0},
+		{0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1},
+		{0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0},
+		{0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0},
+		{0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
+		{1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0},
+		{0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0},
+		{0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1},
+		{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0},
+		{0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0},
+		{0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0},
+		{0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1},
+		{0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0},
+		{0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0},
+		{0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1},
+	});
+	vector<int> ball({27,5});
+	vector<int> hole({2,3});
+
+	string expected_string = "rulululurdrulululululdrulululul";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
+
+TEST( TheMazeIii, Test_drdrdrdldl ) {
+	vector<vector<int>> maze({
+		{0,1,0,0,1,0,0,1,0,0},
+		{0,0,1,0,0,1,0,0,1,0},
+		{0,0,0,0,0,0,1,0,0,1},
+		{0,0,0,0,0,0,1,0,0,1},
+		{0,1,0,0,1,0,0,1,0,0},
+		{0,0,1,0,0,1,0,0,0,0},
+		{0,0,0,0,0,0,1,0,0,0},
+		{1,0,0,1,0,0,0,0,0,1},
+		{0,1,0,0,1,0,0,1,0,0},
+		{0,0,0,0,0,1,0,0,1,0},
+	});
+	vector<int> ball({2,4});
+	vector<int> hole({7,6});
+
+	string expected_string = "drdrdrdldl";
+	string actual_string = Solution().findShortestWay(maze, ball, hole);
+
+	EXPECT_EQ(actual_string, expected_string);
+}
---
+++ the-maze-iii.cpp
@@ -0,0 +1,295 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+//ostream & operator<<( ostream & os, const vector<int> & v ) {
+//	os << "[";
+//	for( size_t i = 0; i < v.size(); i++ ) {
+//		os << v[i];
+//		if ( i + 1 < v.size() ) {
+//			os << ",";
+//		}
+//	}
+//	os << "]";
+//	return os;
+//}
+
+class Solution {
+public:
+
+	// https://leetcode.com/problems/the-maze-iii/
+
+
+	// Analysis
+	//
+	// See analysis in the-maze.cpp
+
+	string findShortestWay( vector<vector<int>>& maze, vector<int> & ball, vector<int> & hole ) {
+
+		vector<vector<int>> cost( maze.size(), vector<int>( maze[0].size(), INT_MAX ));
+		vector<vector<bool>> visited( maze.size(), vector<bool>( maze[0].size(), false ));
+
+		cost[ ball[ 0 ] ][ ball[ 1 ] ] = 0;
+
+//		for( auto & line: maze ) {
+//			cerr << line << endl;
+//		}
+
+//		cerr << "ball (" << ball[ 0 ] << "," << ball[ 1 ] << ")" << endl;
+//		cerr << "hole (" << hole[ 0 ] << "," << hole[ 1 ] << ")" << endl;
+
+		string shortest;
+		helper( maze, ball, hole, NDIR, visited, cost, "", shortest );
+
+		if ( "" == shortest ) {
+			return "impossible";
+		} else {
+			return compress( shortest );
+		}
+	}
+
+protected:
+	enum {
+		UP,
+		RIGHT,
+		DOWN,
+		LEFT,
+		NDIR
+	};
+
+	void helper( const vector<vector<int>> & maze, const vector<int> & ball, const vector<int> & hole, int prev_dir, vector<vector<bool>> & visited, vector<vector<int>> & cost, string path, string & shortest ) {
+
+		//cerr << "(" << ball[ 0 ] << "," << ball[ 1 ] << ") path " << path << endl;
+
+		if ( ball == hole ) {
+
+			if ( "" == shortest ) {
+//				cerr
+//					<< "path " << path << " (" << path.size() << ") beats" << endl
+//					<< "shortest " << shortest << " (" << shortest.size() << ")" << endl
+//					;
+				shortest = path;
+				return;
+			}
+
+			if ( path.size() < shortest.size() ) {
+//				cerr
+//					<< "path     " << path << " (" << path.size() << ") beats" << endl
+//					<< "shortest " << shortest << " (" << shortest.size() << ")" << endl
+//					;
+				shortest = path;
+			} else if ( path.size() == shortest.size() && path < shortest ) {
+//				cerr
+//					<< "path     " << path << " (" << path.size() << ") beats" << endl
+//					<< "shortest " << shortest << " (" << shortest.size() << ")" << endl
+//					;
+				shortest = path;
+			}
+			return;
+		}
+
+		struct EdgeData {
+			int cumulative_cost;
+			int edge_cost;
+			int dir;
+			vector<int> dest;
+			EdgeData() : EdgeData( 0, 0, -1, vector<int>() ) {}
+			EdgeData( int cumulative_cost, int edge_cost, int dir, const vector<int> & dest )
+				: cumulative_cost( cumulative_cost ), edge_cost( edge_cost ), dir( dir ), dest( dest )
+			{}
+		};
+
+
+		struct is_gt {
+			bool operator()( const EdgeData & a, const EdgeData & b ) {
+				return a.cumulative_cost > b.cumulative_cost;
+			}
+		};
+
+		vector<EdgeData> q;
+
+		vector<int> dest = ball; // location of next cell (typically against a wall, or over the hole)
+
+		for( auto dir: vector<int>({ UP, RIGHT, DOWN, LEFT }) ) {
+
+			if ( oppositeDir( prev_dir ) == dir ) {
+				continue;
+			}
+
+			if ( exploreInOneDirection( maze, visited, ball, hole, dir, dest, cost ) ) {
+				EdgeData ed( costAt( cost, dest ), costAt( cost, dest ) - costAt( cost, ball ), dir, dest );
+				q.push_back( ed );
+				push_heap( q.begin(), q.end(), is_gt() );
+			}
+		}
+
+		markVisited( ball, visited );
+
+		for( ; ! q.empty() ;  ) {
+
+			EdgeData e = q.front();
+			pop_heap( q.begin(), q.end(), is_gt() );
+			q.pop_back();
+
+			helper( maze, e.dest, hole, e.dir, visited, cost, path + string( e.edge_cost, dirToChar( e.dir ) ), shortest );
+		}
+	}
+
+	static inline void markVisited( const vector<int> & ball, vector<vector<bool>> & visited ) {
+		visited[ ball[ 0 ] ][ ball[ 1 ] ] = true;
+	}
+
+	static inline int nextRow( int dir, int row ) {
+		switch( dir ) {
+		case UP:   return row-1;
+		case DOWN: return row+1;
+		default: return row;
+		}
+	}
+
+	static inline int nextCol( int dir, int col ) {
+		switch( dir ) {
+		case LEFT:  return col-1;
+		case RIGHT: return col+1;
+		default: return col;
+		}
+	}
+
+	static inline bool updateCost( const vector<int> & dest, const vector<int> & ball, const int & n, vector<vector<int>> & cost ) {
+		if ( cost[ ball[ 0 ] ][ ball[ 1 ] ] + n <= cost[ dest[ 0 ] ][ dest[ 1 ] ] ) {
+			cost[ dest[ 0 ] ][ dest[ 1 ] ] = cost[ ball[ 0 ] ][ ball[ 1 ] ] + n;
+			return true;
+		}
+		return false;
+
+	}
+
+	static bool exploreInOneDirection( const vector<vector<int>> & maze, const vector<vector<bool>> & visited, const vector<int> & ball, const vector<int> & hole, const int dir, vector<int> & dest, vector<vector<int>> & cost ) {
+		int n;
+		int row, col;
+
+		for(
+			n = 0,
+				row = ball[ 0 ],
+				col = ball[ 1 ]
+				;
+			! isWall( maze, nextRow( dir, row ), nextCol( dir, col ) )
+				;
+			n++,
+				row = nextRow( dir, row ),
+				col = nextCol( dir, col )
+		) {
+			// this lets us fall in the hole if we roll over it
+			if ( isHole( row, col, hole ) ) {
+				break;
+			}
+
+			if ( isVisited( visited, row, col ) ) {
+				if ( costAt( cost, ball ) + n <= costAt( cost, vector<int>({row,col}) ) ) {
+					continue;
+				}
+				return false;
+			}
+		}
+		if ( n != 0 ) {
+			dest[ 0 ] = row;
+			dest[ 1 ] = col;
+			return updateCost( dest, ball, n, cost );
+		}
+
+		return false;
+	}
+
+	static inline bool isWall( const vector<vector<int>> & maze, int row, int col ) {
+		if ( row < 0 || size_t( row ) >= maze.size() ) {
+			return true;
+		}
+		if ( col < 0 || size_t( col ) >= maze[ 0 ].size() ) {
+			return true;
+		}
+		return maze[ row ][ col ];
+	}
+
+	static inline bool isVisited( const vector<vector<bool>> & visited, int row, int col ) {
+//		if ( row < 0 || size_t( row ) >= visited.size() ) {
+//			return false;
+//		}
+//		if ( col < 0 || size_t( col ) >= visited[ 0 ].size() ) {
+//			return false;
+//		}
+		return visited[ row ][ col ];
+	}
+
+	static inline bool isHole( const int row, const int col, const vector<int> & hole ) {
+		vector<int> ball({ row, col });
+		return ball == hole;
+	}
+
+	static int costAt( const vector<vector<int>> & cost, const vector<int> & coord ) {
+		return cost[ coord[ 0 ] ][ coord[ 1 ] ];
+	}
+
+	static inline char dirToChar( int dir ) {
+		static const array<char,NDIR> lut({
+			'u',
+			'r',
+			'd',
+			'l',
+		});
+		return lut[ dir ];
+	}
+	static inline int oppositeDir( int dir ) {
+		static const array<int,NDIR+1> lut({
+			DOWN,
+			LEFT,
+			UP,
+			RIGHT,
+			-1,
+		});
+		int r = lut[ dir ];
+		if ( -1 == r ) {
+			r++;
+			r--;
+		}
+		return r;
+	}
+
+	static string compress( const string & s ) {
+		string r;
+		r += s[ 0 ];
+		for( size_t i = 1; i < s.size(); i++ ) {
+			if ( s[ i ] != s[ i - 1 ] ) {
+				r += s[ i ];
+			}
+		}
+		return r;
+	}
+};
