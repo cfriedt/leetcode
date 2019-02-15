59b96186aa3457b68e71a911ba68c475df13ceb7
Christopher Friedt <chrisfriedt@gmail.com>
2019-02-14 20:30:04 -0500

    Implement design-hashset
    https://leetcode.com/problems/design-hashset
    
    time: 144ms
    time-rank: 37%
    time-complexity: O(1)
    
    space: 45.9MB
    space-rank: 100%
    space-complexity: O(N)

---
+++ design-hashset-test.cpp
@@ -0,0 +1,168 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <gtest/gtest.h>
+
+#include "design-hashset.cpp"
+
+/*
+static void graphHistogram( const vector<size_t> & histogram ) {
+
+	enum { MIN, MAX };
+
+	size_t lim[2]{ SIZE_MAX, 0 };
+
+	for( auto & h: histogram ) {
+		if ( h < lim[ MIN ] ) {
+			lim[ MIN ] = h;
+		}
+		if ( h > lim[ MAX ] ) {
+			lim[ MAX ] = h;
+		}
+	}
+
+	size_t M = lim[ MAX ]; // number of rows
+	size_t N = histogram.size(); // number of columns
+
+	vector<vector<bool>> bmp( M, vector<bool>( N, false ) );
+	for( size_t col = 0; col < N; col++ ) {
+		for( size_t row = 0; row < histogram[ col ]; row++ ) {
+			bmp[ lim[ MAX ] - row - 1 ][ col ] = true;
+		}
+	}
+
+	for( size_t row = 0; row < M; row++ ) {
+		for( size_t col = 0; col < N; col++ ) {
+			if ( bmp[ row ][ col ] ) {
+				cerr << 'x';
+			} else {
+				cerr << ' ';
+			}
+		}
+		cerr << endl;
+	}
+	for( size_t col = 0; col < N; col++ ) cerr << '=';
+	cerr << endl;
+}
+*/
+
+size_t histogramNumSamples( const vector<size_t> & histogram ) {
+	size_t n_samples = 0;
+	for( auto & h: histogram ) {
+		n_samples += h;
+	}
+	return n_samples;
+}
+
+TEST( DesignHashset, Test_Example ) {
+	MyHashSet hashSet;
+
+	hashSet.add(1);
+	hashSet.add(2);
+	EXPECT_TRUE( hashSet.contains(1) );
+	EXPECT_FALSE( hashSet.contains(3) );
+	hashSet.add(2);
+	EXPECT_TRUE( hashSet.contains(2) );
+	hashSet.remove(2);
+	EXPECT_FALSE( hashSet.contains(2) );
+}
+
+
+TEST( DesignHashset, Test_remove_nonexistent ) {
+	MyHashSet hashSet;
+	vector<size_t> histogram;
+
+	hashSet.add( 1 );
+	EXPECT_TRUE( hashSet.contains( 1 ) );
+	EXPECT_FALSE( hashSet.contains( 2 ) );
+	hashSet.remove( 2 );
+	EXPECT_FALSE( hashSet.contains( 2 ) );
+	hashSet.remove( 1 );
+	EXPECT_FALSE( hashSet.contains( 1 ) );
+}
+
+TEST( DesignHashset, Test_80Random ) {
+	MyHashSet hashSet;
+	vector<size_t> histogram;
+
+	for( size_t i = 0; i < 80; i++ ) {
+		int x;
+
+		for( x = random(); hashSet.contains( x ); x = random() );
+
+		ASSERT_FALSE( hashSet.contains( x ) );
+
+		hashSet.add( x );
+
+		ASSERT_TRUE( hashSet.contains( x ) );
+
+		histogram = hashSet.histogram();
+
+		ASSERT_EQ( histogramNumSamples( histogram ), i + 1 );
+
+		// graphHistogram( histogram );
+	}
+}
+
+TEST( DesignHashset, Test_1000Random ) {
+	MyHashSet hashSet;
+	vector<size_t> histogram;
+
+	for( size_t i = 0; i < 1000; i++ ) {
+		int x;
+
+		for( x = random(); hashSet.contains( x ); x = random() );
+
+		ASSERT_FALSE( hashSet.contains( x ) );
+
+		hashSet.add( x );
+
+		ASSERT_TRUE( hashSet.contains( x ) );
+
+		histogram = hashSet.histogram();
+
+		ASSERT_EQ( histogramNumSamples( histogram ), i + 1 );
+
+		// graphHistogram( histogram );
+	}
+}
+
+TEST( DesignHashset, Test_1000000Random ) {
+	MyHashSet hashSet;
+
+	for( size_t i = 0; i < 1000000; i++ ) {
+		int x;
+
+		bool add = !!random();
+		x = random();
+
+		if ( add ) {
+			hashSet.add( x );
+		} else {
+			hashSet.remove( x );
+		}
+
+		ASSERT_TRUE( hashSet.contains( x ) );
+	}
+}
---
+++ design-hashset.cpp
@@ -0,0 +1,240 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <functional>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class MyHashSet {
+public:
+
+	// https://leetcode.com/problems/design-hashset
+
+	MyHashSet()
+	: n_items( 0 ), buckets( bucket_block_size, nullptr )
+	{
+	}
+
+	void add( int key ) {
+		add( key, nullptr );
+	}
+
+	void remove( int key ) {
+
+		if ( ! contains( key ) ) {
+			return;
+		}
+
+		size_t hashed_key = hasher( key );
+		uint32_t modded = hashed_key % buckets.size();
+
+		bucket_list_node *b, *b_prev;
+		for( b_prev = nullptr, b = buckets[ modded ]; nullptr != b && b->key != key; b_prev = b, b = b->next );
+		if ( b ) {
+			if ( ! b_prev ) {
+				buckets[ modded ] = b->next;
+			} else {
+				b_prev->next = b->next;
+			}
+			delete b;
+			n_items--;
+			refactor( false );
+		}
+	}
+
+	bool contains( int key ) {
+
+		size_t hashed_key = hasher( key );
+		uint32_t modded = hashed_key % buckets.size();
+
+		bucket_list_node *b;
+
+		for( b = buckets[ modded ]; nullptr != b && b->key != key; b = b->next );
+
+		return nullptr != b;
+	}
+
+	vector<size_t> histogram() {
+
+		vector<size_t> hist;
+
+		for( size_t i = 0; i < buckets.size(); i++ ) {
+			size_t j;
+			bucket_list_node *b;
+			for( b = buckets[ i ], j = 0; nullptr != b; b = b->next ) j++;
+			hist.push_back( j );
+		}
+
+		return hist;
+	}
+
+protected:
+
+	// We're going to use the approach that a hash table is an array of buckets
+	// containing linked lists. There is a hash function that translates the key to
+	// a hashed key, and then the hashed key will be used as an index to say which
+	// bucket / linked-list we are going to add / remove / check for our value.
+	//
+	// A critical measure of the speed of a hash table / set is its load factor.
+	// Which (I believe) is defined as the number of items in the set / the number of
+	// buckets. IIRC, this should definitely be less than unity - I would say less
+	// than 0.5 to be on the safe side.
+	//
+	// Each time our hash table / set grows to a high threshold load factor, we need to
+	// increase its size. Certain sizes might increase collisions, but I'm going to
+	// ignore that for the purpose of this implementation.
+	//
+	// We'll start out with an initial hash size of 64, and when resizing, we'll keep
+	// adding blocks of 64 until our load factor hits a lower threshold.
+	//
+	// Let's make the high threshold load factor 0.5 and the low threshold load factor
+	// 0.25.
+	//
+	// We should always be able to get a histogram of bucket list lengths too, which
+	// will indicate how uniform our hashing algorithm is and how many collisions it
+	// produces for a given number of buckets.
+
+	static constexpr size_t bucket_block_size = 64;
+	static constexpr float load_factor_low  = 0.1;
+	static constexpr float load_factor_high = 0.5;
+
+	static const function<size_t(int)> hasher;
+
+	explicit MyHashSet( size_t n_buckets )
+	: n_items( 0 ), buckets( n_buckets, nullptr )
+	{
+	}
+
+	struct bucket_list_node {
+		int key;
+		size_t hashed_key;
+		bucket_list_node *next;
+		bucket_list_node() : bucket_list_node( 0 ) {}
+		explicit bucket_list_node( int key ) : key( key ), hashed_key( hasher( key ) ), next( nullptr ) {}
+	};
+
+	size_t n_items;
+	vector<bucket_list_node *> buckets;
+
+	void add( int key, bucket_list_node *bucket ) {
+
+		size_t hashed_key;
+		if ( nullptr == bucket ) {
+			hashed_key = hasher( key );
+		} else {
+			key = bucket->key;
+			hashed_key = bucket->hashed_key;
+		}
+
+		uint32_t modded = hashed_key % buckets.size();
+
+		if ( nullptr == buckets[ modded ] ) {
+
+			if ( nullptr == bucket ) {
+				buckets[ modded ] = new bucket_list_node( key );
+			} else {
+				buckets[ modded ] = bucket;
+			}
+
+			n_items++;
+
+			refactor( true );
+
+		} else {
+
+			bucket_list_node *b;
+			for( b = buckets[ modded ]; nullptr != b->next && b->key != key; b = b->next );
+
+			if ( b->key == key ) {
+				return;
+			} else {
+				if ( nullptr == bucket ) {
+					b->next = new bucket_list_node( key );
+				} else {
+					b->next = bucket;
+				}
+			}
+
+			n_items++;
+
+			refactor( true );
+		}
+	}
+
+	void addAll( size_t n_items, vector<bucket_list_node *> & buckets ) {
+		for( size_t i = 0; i < buckets.size() && n_items > 0; i++ ) {
+			vector<bucket_list_node *> stack;
+			for( bucket_list_node *b = buckets[ i ]; b; b = b->next ) {
+				stack.push_back( b );
+			}
+			for( auto & b: stack ) {
+				b->next = nullptr;
+				add( -1, b );
+			}
+		}
+	}
+
+	float loadFactor() {
+		return loadFactor( n_items, buckets.size() );
+	}
+
+	static float loadFactor( size_t n_items, size_t n_buckets ) {
+		return float( n_items ) / n_buckets;
+	}
+
+	void refactor( bool increased ) {
+
+		size_t n_items = this->n_items;
+
+		size_t n_buckets = buckets.size();
+
+		float lf = loadFactor( n_items, n_buckets );
+
+		if ( increased ) {
+			if ( lf >= load_factor_high ) {
+				for( ; lf >= ( load_factor_low + load_factor_high ) / 2; n_buckets += bucket_block_size, lf = loadFactor( n_items, n_buckets ) );
+
+				MyHashSet new_bucket_list( n_buckets );
+				new_bucket_list.addAll( n_items, buckets );
+
+				buckets = new_bucket_list.buckets;
+			}
+		} else {
+			if ( lf < load_factor_low ) {
+				for( ; n_buckets > bucket_block_size && lf < ( load_factor_low + load_factor_high ) / 2; n_buckets -= bucket_block_size, lf = loadFactor( n_items, n_buckets ) );
+
+				if ( 0 != n_buckets ) {
+					MyHashSet new_bucket_list( n_buckets );
+					new_bucket_list.addAll( n_items, buckets );
+
+					buckets = new_bucket_list.buckets;
+				}
+			}
+		}
+	}
+};
+
+const function<size_t(int)> MyHashSet::hasher = hash<int>();
