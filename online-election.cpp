d92df6e94dd832f64f87d590c54873a5bd03e84b
Christopher Friedt <chris.friedt@l3t.com>
2019-10-29 14:47:26 -0400

    Implement online-election
    https://leetcode.com/problems/online-election/submissions/
    
    time: 332ms
    time-rank: 81.84%
    time-complexity: O( N ) for constructor, O( log( N ) ) for q()
    
    space: 75.7MB
    space-rank: 66.67%
    space-complexity: O( N )

---
+++ online-election.cpp
@@ -0,0 +1,139 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+// https://leetcode.com/problems/online-election/
+
+class TopVotedCandidate {
+
+protected:
+
+    struct ent {
+        int time;
+        int candidate; // candidate who received vote at 'time' (i.e. most recent)
+        int leader;
+        int leader_count;
+        ent() : time( 0 ), candidate( 0 ), leader( 0 ), leader_count( 0 ) {}
+        ent( int time, int candidate, int leader, int leader_count /* , unordered_map<int,int> tally  */ )
+        : time( time ), candidate( candidate ), leader( leader ), leader_count( leader_count ) /* , tally( tally ) */
+        {}
+    };
+
+public:
+    TopVotedCandidate(vector<int>& persons, vector<int>& times)
+    : times( times )
+    {
+        size_t N = times.size();
+
+        ea = vector<ent>(N, ent());
+        unordered_map<int,int> tally;
+
+        for( size_t i = 0; i < N; i++ ) {
+            int leader_count;
+            int leader;
+
+            if ( 0 == i ) {
+                leader_count = -1;
+                leader = -1;
+            } else {
+                leader_count = ea[ i - 1 ].leader_count;
+                leader = ea[ i - 1 ].leader;
+            }
+            int candidate = persons[ i ];
+            auto it = tally.find( candidate );
+            if ( tally.end() == it ) {
+                tally[ candidate ] = 1;
+            } else {
+                it->second++;
+            }
+            if ( tally[ candidate ] >= leader_count ) {
+                leader_count = tally[ candidate ];
+                leader = candidate;
+                //cerr << "candidate " << candidate << " took the lead at time " << times[ i ] << " with a score of " << leader_count << endl;
+            }
+            ea[ i ] = ent( times[ i ], candidate, leader, leader_count );
+        }
+    }
+
+    static int compare(int t, int t1, int t2) {
+        if ( t >= t1 && (-1 == t2 || t < t2 ) ) {
+            return 0;
+        }
+        if ( t < t1 ) {
+            return -1;
+        }
+        // -1 != t2 && t >= t2
+        return +1;
+    }
+
+    int q(int t) {
+
+        int i = 0;
+        for( int L = 0, U = times.size() - 1; L <= U; ) {
+            int M = (L + U) / 2;
+            int direction = compare( t, times[ M ], (M < (int)times.size() - 1) ? times[ M + 1 ] : -1 );
+            if ( 0 == direction ) {
+                i = M;
+                break;
+            }
+            if ( direction < 0 ) {
+                U = M;
+            }
+            if ( direction > 0 ) {
+                if ( U - L == 1 && M == L ) {
+                    L = U;
+                } else {
+                    L = M;
+                }
+            }
+        }
+
+        return ea[ i ].leader;
+    }
+
+protected:
+    vector<int> times;
+    vector<ent> ea;
+
+    static unordered_map<int,int> add(const unordered_map<int,int> & a, const unordered_map<int,int> & b) {
+        unordered_map<int,int> c = a;
+        for(auto & kv: b) {
+            c[ kv.first ] += kv.second;
+        }
+        return c;
+    }
+};
+
+/**
+ * Your TopVotedCandidate object will be instantiated and called as such:
+ * TopVotedCandidate* obj = new TopVotedCandidate(persons, times);
+ * int param_1 = obj->q(t);
+ */
