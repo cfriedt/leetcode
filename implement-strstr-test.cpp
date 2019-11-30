b8244d734968b163f764f9ed03ca4321c66ffb1c
Christopher Friedt <chrisfriedt@gmail.com>
2019-11-30 08:35:26 -0500

    improve-code-coverage
    implement-strstr
    string-to-integer-atoi
    pascals-triangle
    divide-two-integers

--- implement-strstr-test.cpp
+++ implement-strstr-test.cpp
@@ -26,7 +26,24 @@
 
 #include "implement-strstr.cpp"
 
-TEST( ImplementStrStr, Test_hello__ll ) {
+TEST( ImplementStrStr, Test_empty_empty ) {
+	string haystack = "";
+	string needle = "";
+	int expected_int = 0;
+	int actual_int = Solution().strStr(haystack, needle);
+	EXPECT_EQ(actual_int, expected_int);
+}
+
+TEST( ImplementStrStr, Test_empty_tiny__yuuuuuge ) {
+	// needle too big
+	string haystack = "tiny";
+	string needle = "yuuuuuge";
+	int expected_int = -1;
+	int actual_int = Solution().strStr(haystack, needle);
+	EXPECT_EQ(actual_int, expected_int);
+}
+
+TEST( ImplementStrStr, Test_hello_ll ) {
 	string haystack = "hello";
 	string needle = "ll";
 	int expected_int = 2;
@@ -34,7 +51,7 @@ TEST( ImplementStrStr, Test_hello__ll ) {
 	EXPECT_EQ(actual_int, expected_int);
 }
 
-TEST( ImplementStrStr, Test_aaaaa__bba ) {
+TEST( ImplementStrStr, Test_aaaaa_bba ) {
 	string haystack = "aaaaa";
 	string needle = "ba";
 	int expected_int = -1;
--- string-to-integer-atoi-test.cpp
+++ string-to-integer-atoi-test.cpp
@@ -26,6 +26,22 @@
 
 #include "string-to-integer-atoi.cpp"
 
+TEST( StringToIntegerAtoi, Test_empty ) {
+	string str("");
+	int expected_int = 0;
+	int actual_int = Solution().myAtoi(str);
+	EXPECT_EQ(actual_int, expected_int);
+}
+
+TEST( StringToIntegerAtoi, Test_preceeding_zeros ) {
+	// preceeding zeros should not add to the full amount in decimal, but should in hex. It isn't really common notation to
+	// have preceeding zeros in any case.
+	string str("010");
+	int expected_int = 10;
+	int actual_int = Solution().myAtoi(str);
+	EXPECT_EQ(actual_int, expected_int);
+}
+
 TEST( StringToIntegerAtoi, Test_42 ) {
 	string str("42");
 	int expected_int = 42;
@@ -57,6 +73,38 @@ TEST( StringToIntegerAtoi, Test_words_and_987 ) {
 TEST( StringToIntegerAtoi, Test_n91283472332 ) {
 	string str("-91283472332");
 	int expected_int = INT_MIN;
+	int actual_int = Solution().myAtoi(str);
+	EXPECT_EQ(actual_int, expected_int);
+}
+
+TEST( StringToIntegerAtoi, Test_n946384712 ) {
+	// ok reverse, but not forward
+	string str("-9463847412");
+	int expected_int = INT_MIN;
+	int actual_int = Solution().myAtoi(str);
+	EXPECT_EQ(actual_int, expected_int);
+}
+
+TEST( StringToIntegerAtoi, Test_8463847412 ) {
+	// ok reverse, but not forward
+	string str("8463847412");
+	int expected_int = INT_MAX;
+	int actual_int = Solution().myAtoi(str);
+	EXPECT_EQ(actual_int, expected_int);
+}
+
+TEST( StringToIntegerAtoi, Test_n9128347233250 ) {
+	// too many decimal digits
+	string str("-9128347233250");
+	int expected_int = INT_MIN;
+	int actual_int = Solution().myAtoi(str);
+	EXPECT_EQ(actual_int, expected_int);
+}
+
+TEST( StringToIntegerAtoi, Test_9128347233250 ) {
+	// too many decimal digits
+	string str("9128347233250");
+	int expected_int = INT_MAX;
 	int actual_int = Solution().myAtoi(str);
 	EXPECT_EQ(actual_int, expected_int);
 }
