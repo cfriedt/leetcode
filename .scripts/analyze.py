dd38f2a2c118a9cff6c32c0bf20d2b377eb9e369
Christopher Friedt <chrisfriedt@gmail.com>
2019-12-10 19:57:06 -0500

    list of time_ranks, space_ranks, and problems with malformed commit messages

--- .scripts/analyze.py
+++ .scripts/analyze.py
@@ -4,69 +4,77 @@ import git
 import glob
 import sys
 
-class GitAnalyzer(object):
-
-    def __init__(self):
-        self.repo = git.Repo('.')
-        self.commits = list( self.repo.iter_commits( 'master' ) )
-
-    def is_exists(self, filename, sha):
-        """Check if a file in current commit exist."""
-        files = self.repo.git.show("--pretty=", "--name-only", sha)
-        if filename in files:
-            return True
-    
-    def get_file_commits(self, filename):
-        file_commits = []
-        for commit in self.commits:
-            if self.is_exists(filename, commit.hexsha):
-                file_commits.append(commit)
-    
-        return file_commits
-
 def getProblemList():
     problems = []
     problems_tmp = glob.glob( '*-test.cpp' )
     for p in problems_tmp:
         p = p[:-9]
         problems.append( p )
-    return problems 
+    return problems
 
 def main( arg ):
-    
+
     retval = 0
-    
+
+    time_ranks = []
+    space_ranks = []
+    malformed = []
+
     problem_fixups = { 'peak-index-in-mountain-array': 'peak-index-in-a-mountain-array' }
-    
-    
-    ga = GitAnalyzer()
+
     problems = getProblemList()
-    
+
+    repo = git.Repo('.')
+
     for p in problems:
-        
+
         filename = p + '.cpp'
-        
-        if p in problem_fixups:
-            p = problem_fixups[ p ]
-        
-        file_commits = ga.get_file_commits(filename)
-        
+
         leetcode_stats_commit = None
-        for fc in file_commits:
-            msg = fc.message
-            if 'https://leetcode.com/problems/' + p in msg or 'Implement ' + p in msg:
-                leetcode_stats_commit = fc
-                break
+
+        for commit in repo.iter_commits( paths = filename ):
+
+                msg = commit.message
+
+                if 'https://leetcode.com/problems/' + p in msg or 'Implement ' + p in msg:
+                        if 'time-rank: ' in msg and 'space-rank: ' in msg:
+                            time_rank = 0.0
+                            space_rank = 0.0
+                            for line in msg.splitlines():
+                                if line.startswith( 'time-rank: ' ):
+                                    words = line.split()
+                                    time_rank = words[ 1 ]
+                                    time_rank = time_rank.replace( '%', '' )
+                                    try:
+                                        time_rank = float( time_rank )
+                                    except:
+                                        break
+                                    time_ranks.append( time_rank )
+                                if line.startswith( 'space-rank: ' ):
+                                    words = line.split()
+                                    space_rank = words[ 1 ]
+                                    space_rank = space_rank.replace( '%', '' )
+                                    try:
+                                        space_rank = float( space_rank )
+                                    except:
+                                        break
+                                    space_ranks.append( space_rank )
+                            print( '{}: time-rank: {} space-rank: {}'.format( p, time_rank, space_rank ) )
+                            leetcode_stats_commit = commit
+                            break
 
         if leetcode_stats_commit is None:
-            print( 'failed to find stats commit for {}'.format( filename ) )
+            #print( 'malformed commit for problem {}'.format( p ) )
             retval = 1
+            malformed.append( p )
             continue
-            
-        #print( '{}:\n{}'.format( filename, leetcode_stats_commit.message ) )
-        
+
+    print( 'time_ranks: {}: {}'.format( len( time_ranks ), time_ranks ) )
+    print( 'space_ranks: {}: {}'.format( len( space_ranks ), space_ranks ) )
+    print( 'malformed: {}: {}'.format( len( malformed ), malformed ) )
+
     return retval
-    
+
 if '__main__' == __name__:
     sys.exit( main( sys.argv ) )
-  
+
