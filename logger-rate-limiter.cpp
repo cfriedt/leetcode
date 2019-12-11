5fb42d53fdab0413474e88e6b610eb5ad9b85ab8
Christopher Friedt <chrisfriedt@gmail.com>
2019-12-11 06:47:03 -0500

    Implement logger-rate-limiter
    https://leetcode.com/problems/logger-rate-limiter
    
    time: 96ms
    time-rank: 64.51%
    time-complexity: O( 1 )
    
    space: 32.7MB
    space-rank: 100.00%
    space-complexity: O( N )

---
+++ logger-rate-limiter.cpp
@@ -0,0 +1,64 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+// https://leetcode.com/problems/logger-rate-limiter/
+
+class Logger {
+public:
+
+    unordered_map<string,int> hysteresis;
+
+    /** Initialize your data structure here. */
+    Logger() {
+
+    }
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        auto it = hysteresis.find( message );
+        if ( hysteresis.end() != it ) {
+            if ( timestamp - it->second < 10 ) {
+                return false;
+            }
+            it->second = timestamp;
+            return true;
+        }
+
+        hysteresis[ message ] = timestamp;
+        return true;
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
