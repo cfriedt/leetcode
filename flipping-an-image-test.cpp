6ab3934e97a5248c5d7b548cf0102174854f52bd
Christopher Friedt <chrisfriedt@gmail.com>
2020-12-31 08:57:30 -0500

    Implement flipping-an-image
    https://leetcode.com/problems/flipping-an-image
    
    time: 8ms
    time-rank: 71.19%
    time-complexity: O(N^2)
    
    space: 9.3MB
    space-rank: 67.95%
    space-complexity: O(1)
    
    Fixes #236
    
    Could probably be improved by not using std::reverse(). Doing so effectively doubles the execution time.
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

---
+++ flipping-an-image-test.cpp
@@ -0,0 +1,59 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <gtest/gtest.h>
+
+#include "flipping-an-image.cpp"
+
+TEST(FlippingAnImage, 1_1_0__1_0_1__0_0_0) {
+  vector<vector<int>> A = {
+      {1, 1, 0},
+      {1, 0, 1},
+      {0, 0, 0},
+  };
+  vector<vector<int>> expected = {
+      {1, 0, 0},
+      {0, 1, 0},
+      {1, 1, 1},
+  };
+
+  EXPECT_EQ(expected, Solution().flipAndInvertImage(A));
+}
+
+TEST(FlippingAnImage, 1_1_0_0__1_0_0_1__0_1_1_1__1_0_1_0) {
+  vector<vector<int>> A = {
+      {1, 1, 0, 0},
+      {1, 0, 0, 1},
+      {0, 1, 1, 1},
+      {1, 0, 1, 0},
+  };
+  vector<vector<int>> expected = {
+      {1, 1, 0, 0},
+      {0, 1, 1, 0},
+      {0, 0, 0, 1},
+      {1, 0, 1, 0},
+  };
+
+  EXPECT_EQ(expected, Solution().flipAndInvertImage(A));
+}
