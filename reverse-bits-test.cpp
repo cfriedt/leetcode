7e414250dc864380547bcd33aab0d479ff76ecb5
Christopher Friedt <chrisfriedt@gmail.com>
2021-01-01 11:49:59 -0500

    Implement reverse-bits
    https://leetcode.com/problems/reverse-bits
    
    time: 4ms
    time-rank: 53.00%
    time-complexity: O(1)
    
    space: 6.1MB
    space-rank: 86.26%
    space-complexity: O(1)
    
    Fixes #245
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

---
+++ reverse-bits-test.cpp
@@ -0,0 +1,37 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <gtest/gtest.h>
+
+#include "reverse-bits.cpp"
+
+TEST(ReverseBits, 00000010100101000001111010011100) {
+  EXPECT_EQ(964176192,
+            Solution().reverseBits(0b00000010100101000001111010011100));
+}
+
+TEST(ReverseBits, 11111111111111111111111111111101) {
+  EXPECT_EQ(3221225471,
+            Solution().reverseBits(0b11111111111111111111111111111101));
+}
---
+++ reverse-bits.cpp
@@ -0,0 +1,38 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+class Solution {
+public:
+  uint32_t reverseBits(uint32_t n) {
+    static const uint8_t lut[] = {
+        0b0000, 0b1000, 0b0100, 0b1100, 0b0010, 0b1010, 0b0110, 0b1110,
+        0b0001, 0b1001, 0b0101, 0b1101, 0b0011, 0b1011, 0b0111, 0b1111,
+    };
+
+    return 0 | (lut[(n >> 0) & 0xf] << 28) | (lut[(n >> 4) & 0xf] << 24) |
+           (lut[(n >> 8) & 0xf] << 20) | (lut[(n >> 12) & 0xf] << 16) |
+           (lut[(n >> 16) & 0xf] << 12) | (lut[(n >> 20) & 0xf] << 8) |
+           (lut[(n >> 24) & 0xf] << 4) | (lut[(n >> 28) & 0xf] << 0);
+  }
+};
