6c6d8264aedf514b0ca14232b7ccffe83135cd7e
Christopher Friedt <chrisfriedt@gmail.com>
2021-01-02 11:52:15 -0500

    Implement max-points-on-a-line
    https://leetcode.com/problems/max-points-on-a-line
    
    time: 48ms
    time-rank: 33.88%
    time-complexity: O(N^2)
    
    space: 12MB
    space-rank: 30.96%
    space-complexity: O(N^2)
    
    Fixes #244
    
    This iteration improves things slightly by going from O(N^3) to O(N^2) in time.
    We use doubles for quanifying slopes and intercepts, but the theory is that
    those can be further improved by using Q numbers (a.k.a. rationals) instead of
    doubles. Will save that improvement for the next iteration on this problem.
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

--- max-points-on-a-line.cpp
+++ max-points-on-a-line.cpp
@@ -46,11 +46,37 @@ template <> struct hash<Point> {
 };
 } // namespace std
 
+struct Slope {
+  Slope() : Slope(0) {}
+  Slope(Point a, Point b) : Slope(0) {
+    int num = b.y - a.y;
+    int den = b.x - a.x;
+    if (den == 0) {
+      m = numeric_limits<double>::infinity();
+    } else {
+      m = double(num) / double(den);
+    }
+  }
+  Slope(double m) : m(m) {}
+  double m;
+  bool operator==(const Slope &o) const { return m == o.m; }
+};
+
+namespace std {
+template <> struct hash<Slope> {
+  size_t operator()(const Slope &m) const { return hash<double>()(m.m); }
+};
+} // namespace std
+
 struct Intercept {
   Intercept() : Intercept(0, 0) {}
   Intercept(Point a, Point b) : Intercept(0, 0) {
     m = double(b.y - a.y) / double(b.x - a.x);
     y = a.y - m * a.x;
+  }
+  Intercept(Point a, Slope m) : Intercept(0, 0) {
+    this->m = m.m;
+    y = a.y - this->m * a.x;
   }
   Intercept(double y, double m) : y(y), m(m) {}
   double y;
@@ -70,101 +96,56 @@ template <> struct hash<Intercept> {
 class Solution {
 public:
   int maxPoints(vector<vector<int>> &points) {
-    // in one loop: O(N) - first order
-    // - increment entries in an undered_map<Point,size_t> "count" to account
-    // for duplicates
-    // - add points to an unordered_map<int,size_t> with the X value of each
-    // point acting as the key
-    //   - that is sufficient to describe vertical lines
-    //   - maintain a maxpoints variable that is updated with each new addition
-    // - add points to an unordered_map<int,size_t> with the Y value of each
-    // point acting as the key
-    //   - that is sufficient to describe horizontal lines
-    //   - maintain a maxpoints variable that is updated with each new addition
-    //
-    // shortcut: O(1)
-    // - here, if mapoints == points.size(), we can exit early
-    //
-    // - create an unordered_map<Intercept, size_t> "icept"
-    //
-    // in the next loop: O(N^3) - higher-order
-    // - for each unique pair (2-tuple) of points in count: O(N^2)
-    //   - if points are equal, do not generate the pair, they can be skipped
-    //   - if points share an X or Y coordinate (i.e. are vertical or
-    //   horizontal) they can be skipped
-    //   - from the pair of points, a point + unit vector can be extracted
-    //   - but more importantly, a y-intercept plus slope can be extracted
-    //   (let's just call this the intercept)
-    //   - that is sufficient to describe diagonal lines
-    //   - if a particular intercept exists in icept the map already, then
-    //   continue
-    //   - for each other unique point, check if an identical intercept with one
-    //   of the earlier points can be made: O(N)
-    //     - if so, increment icept for the current intercept
-    //     - maintain a maxpoints variable that is updated with each new
-    //     addition
-    //
-    //
-    // Given that this O(N^3) solutions are rarely ever seen as a successful
-    // (even though this one works) I would probably be inclined to wonder about
-    // possible DP solutions.
-
     unordered_map<Point, size_t> count;
-    unordered_map<int, size_t> vert;
-    unordered_map<int, size_t> horiz;
-    unordered_map<Intercept, size_t> icept;
     size_t maxpoints = 0;
 
     for (auto &p : points) {
       int x = p[0];
       int y = p[1];
       count[Point(x, y)]++;
-
-      vert[x]++;
-      maxpoints = max(maxpoints, vert[x]);
-
-      horiz[y]++;
-      maxpoints = max(maxpoints, horiz[y]);
     }
 
-    if (maxpoints == points.size()) {
-      return maxpoints;
+    vector<Point> keys;
+    for (auto kv : count) {
+      keys.push_back(kv.first);
     }
 
-    for (auto aa : count) {
-      for (auto bb : count) {
-        auto a = aa.first;
-        auto b = bb.first;
+    const size_t N = keys.size();
 
-        if (a == b) {
+    if (keys.size() == 1) {
+      return count[keys[0]];
+    }
+
+    unordered_map<Intercept, size_t> icept;
+
+    for (size_t i = 0; i < N; ++i) {
+      unordered_map<Slope, size_t> with_slope;
+      for (size_t j = i + 1; j < N; ++j) {
+        auto ic = Intercept(keys[i], keys[j]);
+        if (icept.end() != icept.find(ic)) {
           continue;
         }
-        if (a.x == b.x || a.y == b.y) {
-          continue;
+        auto m = Slope(keys[i], keys[j]);
+        if (with_slope[m] == 0) {
+          with_slope[m] += count[keys[i]];
         }
-        auto x = Intercept(a, b);
-        if (icept.end() != icept.find(x)) {
-          continue;
-        }
+        with_slope[m] += count[keys[j]];
+        // cout
+        //     << "a: (" << keys[i].x << ", " << keys[i].y << ") "
+        //     << "-> "
+        //     << "b: (" << keys[j].x << ", " << keys[j].y << ") "
+        //     << "m: " << float(m.m)
+        //     << ": with_slope[m]: " << with_slope[m] << endl;
+        maxpoints = max(maxpoints, with_slope[m]);
+      }
 
-        icept[x] += count[a] + count[b];
-        maxpoints = max(maxpoints, icept[x]);
-
-        for (auto cc : count) {
-          auto c = cc.first;
-          if (a == c || b == c) {
-            continue;
-          }
-          auto t = Intercept(a, c);
-          if (t != x) {
-            continue;
-          }
-
-          icept[x] += count[c];
-          maxpoints = max(maxpoints, icept[x]);
-        }
+      for (auto kv : with_slope) {
+        auto m = kv.first;
+        auto n = kv.second;
+        icept[Intercept(keys[i], m)] = n;
       }
     }
+
     return maxpoints;
   }
 };
