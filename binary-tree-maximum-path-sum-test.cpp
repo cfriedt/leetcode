0a88eca57ad56184891a7f8a3a583081bdb334f9
Christopher Friedt <chrisfriedt@gmail.com>
2021-01-06 20:11:46 -0500

    Implement binary-tree-maximum-path-sum
    https://leetcode.com/problems/binary-tree-maximum-path-sum
    
    time: 36ms
    time-rank: 60.28%
    time-complexity: O(N)
    
    space: 29.1MB
    space-rank: 31.82%
    space-complexity: O(1), O(N) when counting stack space
    
    Fixes #255
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

---
+++ binary-tree-maximum-path-sum-test.cpp
@@ -0,0 +1,69 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Christopher Friedt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <gtest/gtest.h>
+
+#include "util/TreeNode.cpp"
+#include "binary-tree-maximum-path-sum.cpp"
+
+using namespace std;
+
+TEST(BinaryTreeMaximumPathSum, 1_2_3) {
+  TreeNode *root = TreeNode_from_string("[1, 2, 3]");
+  EXPECT_EQ(6, Solution().maxPathSum(root));
+}
+
+TEST(BinaryTreeMaximumPathSum, n10_9_20_null_null_15_7) {
+  TreeNode *root = TreeNode_from_string("[-10, 9, 20, null, null, 15, 7]");
+  EXPECT_EQ(42, Solution().maxPathSum(root));
+}
+
+TEST(BinaryTreeMaximumPathSum, n3) {
+  TreeNode *root = TreeNode_from_string("[-3]");
+  EXPECT_EQ(-3, Solution().maxPathSum(root));
+}
+
+TEST(BinaryTreeMaximumPathSum, 1_n2_n3_1_3_n2_null_n1) {
+  TreeNode *root = TreeNode_from_string("[1,-2,-3,1,3,-2,null,-1]");
+  EXPECT_EQ(3, Solution().maxPathSum(root));
+}
+
+// This actually seems to be a kind of bug in LeetCode's binary tree builder
+// Basically, if a BT node is NULL, then it's children don't need to be
+// specified, even if there are valid nodes following. Seems a little
+// inconsistent.
+// See also util/TreeNode-test.cpp
+#if 0
+TEST(BinaryTreeMaximumPathSum, 5_4_8_11_null_13_4_7_2_null_null_null_1) {
+  TreeNode *root = TreeNode_from_string("[5,4,8,11,null,13,4,7,2,null,null,null,1]");
+  EXPECT_EQ(48, Solution().maxPathSum(root));
+}
+#else
+TEST(BinaryTreeMaximumPathSum,
+     5_4_8_11_null_13_4_7_2_null_null_null_null_null_1) {
+  TreeNode *root = TreeNode_from_string(
+      "[5,4,8,11,null,13,4,7,2,null,null,null,null,null,1]");
+  EXPECT_EQ(48, Solution().maxPathSum(root));
+}
+#endif
