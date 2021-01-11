241d3be064ec833b6e52596ca96f0d28162b18d6
Christopher Friedt <chrisfriedt@gmail.com>
2021-01-10 23:43:39 -0500

    Implement minimum-deletion-cost-to-avoid-repeating-letters
    name: minimum-deletion-cost-to-avoid-repeating-letters
    url: https://leetcode.com/problems/minimum-deletion-cost-to-avoid-repeating-letters
    difficulty: medium
    
    time: 264ms
    time-rank: 95.54%
    time-complexity: O(N)
    
    space: 95.9MB
    space-rank: 24.79%
    space-complexity: O(1)
    
    Fixes #280
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

---
+++ minimum-deletion-cost-to-avoid-repeating-letters-test.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021, Christopher Friedt
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <gtest/gtest.h>
+
+#include "minimum-deletion-cost-to-avoid-repeating-letters.cpp"
+
+using namespace std;
+
+TEST(MinimumDeletionCostToAvoidRepeatingLetters, abaac__1_2_3_4_5) {
+	string s = "abaac";
+	vector<int> cost = {
+		1, 2, 3, 4, 5
+	};
+	EXPECT_EQ(3, Solution().minCost(s, cost));
+}
+
+TEST(MinimumDeletionCostToAvoidRepeatingLetters, abc__1_2_3) {
+	string s = "abc";
+	vector<int> cost = {
+		1, 2, 3,
+	};
+	EXPECT_EQ(0, Solution().minCost(s, cost));
+}
+
+TEST(MinimumDeletionCostToAvoidRepeatingLetters, aabaa__1_2_3_4_1) {
+	string s = "aabaa";
+	vector<int> cost = {
+		1, 2, 3, 4, 1
+	};
+	EXPECT_EQ(2, Solution().minCost(s, cost));
+}
+
